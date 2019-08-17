c56b4538f685e471d37991f21aee7c8ceb6126dc
Marie <marie-sarah@live.com>
2019-08-17 17:06:19 -0400

    Initial commit + minor clean up.

---
+++ src/app.jsx
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import './scss/app.scss';
+
+function App() {
+  return (
+<body>
+	<div className="container">
+		<div className="container-row">
+		</div>
+	</div>
+	<footer>
+
+	</footer>
+</body>
+	);
+}
+
+export default App;
+
